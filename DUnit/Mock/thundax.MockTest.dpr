69a4c0e92c40771ec79c61f5f9865ecb36674b8f
jordi.coll.corbilla@gmail.com <jordi.coll.corbilla@gmail.com@a271f7d6-0a02-cd55-3d11-3771d1d4ca8d>
2012-05-26 00:52:35 +0000

    Mock Example using anonymous methods

---
+++ DUnit/Mock/thundax.MockTest.dpr
@@ -0,0 +1,36 @@
+program thundax.MockTest;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  thundax.mock in '..\..\src\Mock\thundax.mock.pas',
+  thundax.mock.example in '..\..\src\Mock\thundax.mock.example.pas',
+  TestThundax.mock in 'TestThundax.mock.pas';
+
+{R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
