e7f674d7fcdedf4740f4e612edb3535a43de5d68
jordi.coll.corbilla@gmail.com <jordi.coll.corbilla@gmail.com@a271f7d6-0a02-cd55-3d11-3771d1d4ca8d>
2013-06-11 20:17:46 +0000

    Adding code coverage display using EMMA

--- DUnit/Design Patterns/dcov_execute.bat
+++ DUnit/Design Patterns/dcov_execute.bat
@@ -1,1 +1,1 @@
-CodeCoverage.exe -e "thundax.DesignPatternsTest.exe" -m "thundax.DesignPatternsTest.map" -ife -uf dcov_units.lst -spf dcov_paths.lst -od ".\" -lt -html -xml -emma
+CodeCoverage.exe -e "thundax.DesignPatternsTest.exe" -m "thundax.DesignPatternsTest.map" -ife -uf dcov_units.lst -spf dcov_paths.lst -od ".\" -lt -html -xml -emma -meta
